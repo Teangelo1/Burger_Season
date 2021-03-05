f79841d9ee4d86e3bb8054dc370b534a033c1b10
Teangelo Burks <burks.teangelo@gmail.com>
2021-03-04 21:08:04 -0500

    Added queries for selecting All, inserting, and updating our tables within the database

--- config/orm.js
+++ config/orm.js
@@ -1,2 +1,53 @@
 // Importing MySQL connection here
 const connection = require('./connection.js');
+
+const orm = {
+    selectAll(tableInput, cb) {
+        const queryString = `SELECT * FROM ${tableInput};`;
+        connection.query(queryString, (err, result) => {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    insertOne(table, cols, vals, cb) {
+        let queryString = `INSERT INTO ${table}`;
+
+        queryString += ' (';
+        queryString += cols.toString();
+        queryString += ') ';
+        queryString += 'VALUES (';
+        queryString += printQuestionMarks(vals.length);
+        queryString += ') ';
+
+        console.log(queryString);
+
+        connection.query(queryString, vals, (err, result) => {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    },
+
+    update(table, objColVals, condition, cb) {
+        let queryString = `UPDATE ${table}`;
+
+        queryString += ' SET ';
+        queryString += objToSql(objColVals);
+        queryString += ' WHERE ';
+        queryString += condition;
+
+        console.log(queryString);
+        connection.query(queryString, (err, result) => {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    }
+
+}
