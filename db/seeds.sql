281e0baea746378f872ca8262dde448de337f557
Teangelo Burks <burks.teangelo@gmail.com>
2021-03-05 19:07:32 -0500

    Inserted options in the database with a boolean value to for furthing testing when time to render content to the page

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Cheeseburger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Steak Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Barbeque Cheese Burger", true);
